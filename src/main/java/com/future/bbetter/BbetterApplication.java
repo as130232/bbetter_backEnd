7e76dff16fdb4ee35866d7ce9918d1d2ad784e03
Charles-PC\Charles <as130232@gmail.com>
2017-08-26 16:15:44 +0800

    feat bbetter spring boot project version 1.0

---
+++ src/main/java/com/future/bbetter/BbetterApplication.java
@@ -0,0 +1,12 @@
+package com.future.bbetter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BbetterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BbetterApplication.class, args);
+	}
+}
---
+++ src/test/java/com/future/bbetter/BbetterApplicationTests.java
@@ -0,0 +1,16 @@
+package com.future.bbetter;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BbetterApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
