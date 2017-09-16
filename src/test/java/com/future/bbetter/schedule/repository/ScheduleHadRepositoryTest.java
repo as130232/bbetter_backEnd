174e0e0618a437708b85e85ffd90aa89aca126e9
alfreadx <alfreadx@gmail.com>
2017-09-16 10:38:00 +0100

    改為使用程式動態插入資料來執行測試

--- src/test/java/com/future/bbetter/schedule/repository/ScheduleHadRepositoryTest.java
+++ src/test/java/com/future/bbetter/schedule/repository/ScheduleHadRepositoryTest.java
@@ -2,8 +2,13 @@ package com.future.bbetter.schedule.repository;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
+import java.util.Date;
 import java.util.List;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,7 +18,13 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.autoconfigure.orm.jpa.TestEntityManager;
 import org.springframework.test.context.junit4.SpringRunner;
 
+import com.future.bbetter.member.constant.MEMBER;
+import com.future.bbetter.member.model.Member;
+import com.future.bbetter.schedule.model.Schedule;
+import com.future.bbetter.schedule.model.ScheduleDTO;
 import com.future.bbetter.schedule.model.ScheduleHad;
+import com.future.bbetter.schedule.model.ScheduleSubType;
+import com.future.bbetter.schedule.model.ScheduleType;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -29,21 +40,99 @@ public class ScheduleHadRepositoryTest {
 	@Autowired
 	private ScheduleHadRepository schHadRepo;
 	
+	private Long john_id = 0L;
+	private Long jay_id = 0L;
+	private Long schedule1_id = 0L;
+	private Long schedule2_id = 0L;
+	
+	
+	@Before
+	public void setup(){
+		//add member data
+		Member john = new Member();
+		john.setName("john");
+		john.setEmail("john@gmail.com");
+		john.setPassword("password");
+		john.setGender(1);
+		john.setMoney(0.0);
+		john.setAddress("North Korea");
+		entityMgr.persistAndFlush(john);
+		
+		Member jay = new Member();
+		jay.setName("jay");
+		jay.setEmail("jay@gmail.com");
+		jay.setPassword("password");
+		jay.setGender(1);
+		jay.setMoney(100.0);
+		jay.setAddress("South Korea");
+		entityMgr.persistAndFlush(jay);
+		
+		john_id = john.getMemberId();
+		jay_id = jay.getMemberId();
+		
+		//add schedule type data
+		ScheduleType type = new ScheduleType();
+		type.setName("test_type");
+		entityMgr.persistAndFlush(type);
+		
+		//add schedule sub type data
+		ScheduleSubType subType = new ScheduleSubType();
+		subType.setName("test_subtype");
+		subType.setScheduleTypeId(type.getScheduleTypeId());
+		entityMgr.persistAndFlush(subType);
+		
+		//add schedule data
+		Instant now = Instant.now();
+		Instant afterTwoHrs = LocalDateTime.now().plusHours(2)
+				.toInstant(ZoneOffset.UTC);
+		Instant afterFourHrs = LocalDateTime.now().plusHours(4)
+				.toInstant(ZoneOffset.UTC);
+		
+		Schedule schedule1= new Schedule();
+		schedule1.setScheduleSubTypeId(subType.getScheduleSubTypeId());
+		schedule1.setStartTime(Date.from(now));
+		schedule1.setEndTime(Date.from(afterTwoHrs));
+		schedule1.setName("Test_Schedule");
+		schedule1.setStatus(1);
+		schedule1.setVisibility(2);
+		schedule1.setIsCycle(0);
+		schedule1.setIsNeedRemind(0);
+		schedule1.setIsTeamSchedule(0);
+		schedule1.setIsValid(0);
+		entityMgr.persistAndFlush(schedule1);
+		
+		
+		Schedule schedule2= new Schedule();
+		schedule2.setScheduleSubTypeId(subType.getScheduleSubTypeId());
+		schedule2.setStartTime(Date.from(now));
+		schedule2.setEndTime(Date.from(afterFourHrs));
+		schedule2.setName("Test_Schedule");
+		schedule2.setStatus(1);
+		schedule2.setVisibility(2);
+		schedule2.setIsCycle(0);
+		schedule2.setIsNeedRemind(0);
+		schedule2.setIsTeamSchedule(0);
+		schedule2.setIsValid(0);
+		entityMgr.persistAndFlush(schedule2);
+		
+		schedule1_id = schedule1.getScheduleId();
+		schedule2_id = schedule2.getScheduleId();
+	}
 	
 	@Test
-	public void whenFindByScheduleId_thenReturnRecords(){
+	public void whenFindByScheduleId_thenReturnTwoDifferentRecords(){
 		//given
 		ScheduleHad had = new ScheduleHad();
-		had.setMemberId(1L);
-		had.setScheduleId(3L);
+		had.setMemberId(john_id);
+		had.setScheduleId(schedule1_id);
 		had.setAuthority(1);
 		had.setIsValid(1);
 		had.setAccumulatedTime(0);
 		entityMgr.persistAndFlush(had);
 		
 		ScheduleHad had2 = new ScheduleHad();
-		had2.setMemberId(2L);
-		had2.setScheduleId(3L);
+		had2.setMemberId(jay_id);
+		had2.setScheduleId(schedule1_id);
 		had2.setAuthority(1);
 		had2.setIsValid(1);
 		had2.setAccumulatedTime(0);
@@ -51,14 +140,49 @@ public class ScheduleHadRepositoryTest {
 		
 		
 		//when
-		List<ScheduleHad> founds =  schHadRepo.findByScheduleId(3L);
+		List<ScheduleHad> founds =  schHadRepo.findByScheduleId(schedule1_id);
 		
 		//then
 		assertThat(founds.size()).isEqualTo(2);
 
 		for(ScheduleHad found : founds){
 			assertThat(found.getScheduleId())
-			.isEqualTo(had.getScheduleId());
+				.isEqualTo(had.getScheduleId());
+		}
+		
+		assertThat(founds.get(0).getScheduleHadId())
+			.isNotEqualTo(founds.get(1).getScheduleHadId());
+	}
+	
+	
+	@Test
+	public void whenFindByMemberId_thenReturnTwoDifferentRecords(){
+		//given
+		ScheduleHad had = new ScheduleHad();
+		had.setMemberId(john_id);
+		had.setScheduleId(schedule1_id);
+		had.setAuthority(1);
+		had.setIsValid(1);
+		had.setAccumulatedTime(0);
+		entityMgr.persistAndFlush(had);
+		
+		ScheduleHad had2 = new ScheduleHad();
+		had2.setMemberId(john_id);
+		had2.setScheduleId(schedule2_id);
+		had2.setAuthority(1);
+		had2.setIsValid(1);
+		had2.setAccumulatedTime(0);
+		entityMgr.persistAndFlush(had2);
+		
+		
+		//when
+		List<ScheduleHad> founds =  schHadRepo.findByMemberId(john_id);
+		//then
+		assertThat(founds.size()).isEqualTo(2);
+
+		for(ScheduleHad found : founds){
+			assertThat(found.getMemberId())
+				.isEqualTo(had.getMemberId());
 		}
 		
 		assertThat(founds.get(0).getScheduleHadId())
