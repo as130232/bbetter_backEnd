e4ccf59e125e73a1c3b6792d2ffc258558a60687
Charles-PC\Charles <as130232@gmail.com>
2017-09-19 22:06:30 +0800

    feat 新增Controller驗證錯誤時處理驗證Exception

--- src/main/java/com/future/bbetter/exception/GobalExceptionHandler.java
+++ src/main/java/com/future/bbetter/exception/GobalExceptionHandler.java
@@ -4,6 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.http.HttpStatus;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -15,7 +16,11 @@ import com.future.bbetter.exception.model.ErrorInfo;
 @ControllerAdvice
 @RestController
 public class GobalExceptionHandler {
-
+	/**
+	 * 參數驗證失敗
+	 * @author Charles
+	 * @date 2017年9月17日 上午11:05:59
+	 */
 	@ExceptionHandler(value = ValidateFailException.class)
 	//狀態碼用來表示此次請求的結果(成功、失敗、其他原因等)
 	@ResponseStatus(HttpStatus.PRECONDITION_FAILED)
@@ -28,6 +33,11 @@ public class GobalExceptionHandler {
 		return error;
 	}
 
+	/**
+	 * 數據庫中找不到該值
+	 * @author Charles
+	 * @date 2017年9月17日 上午11:05:59
+	 */
 	@ExceptionHandler(value = DataNotFoundException.class)
 	@ResponseStatus(HttpStatus.NOT_FOUND)
 	public ErrorInfo<String> dataNotFoundHandler(HttpServletRequest req, HttpServletResponse res,
@@ -39,4 +49,19 @@ public class GobalExceptionHandler {
 		return error;
 	}
 
+	/**
+	 * 登入時驗證失敗
+	 * @author Charles
+	 * @date 2017年9月17日 上午11:05:59
+	 */
+	@ExceptionHandler(value = AuthenticationException.class)
+	@ResponseStatus(HttpStatus.UNAUTHORIZED)
+	public ErrorInfo<String> AuthenticationHandler(HttpServletRequest req, HttpServletResponse res,
+			AuthenticationException e) {
+		ErrorInfo<String> error = new ErrorInfo<>();
+		error.setCode(HttpStatus.UNAUTHORIZED.value());
+		error.setMessage(e.getMessage());
+		error.setUrl(req.getRequestURL().toString());
+		return error;
+	}
 }
