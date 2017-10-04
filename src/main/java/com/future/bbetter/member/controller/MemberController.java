dc654d3a0fbf3ed40eeda85806032656b596f252
Charles-PC\Charles <as130232@gmail.com>
2017-10-04 16:05:29 +0800

    fix member 修正會員DTO feat member 更新會員API接口 feat member 新增好友DTO、新增取得好友API接口、取得好友resource

--- src/main/java/com/future/bbetter/member/controller/MemberController.java
+++ src/main/java/com/future/bbetter/member/controller/MemberController.java
@@ -1,8 +1,6 @@
 package com.future.bbetter.member.controller;
 
-import java.util.Enumeration;
 import java.util.List;
-import java.util.Locale;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -10,6 +8,8 @@ import javax.servlet.http.HttpServletResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -18,13 +18,14 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.future.bbetter.exception.customize.DataNotFoundException;
-import com.future.bbetter.exception.customize.ValidateFailException;
+import com.future.bbetter.member.model.FriendDTO;
 import com.future.bbetter.member.model.MemberDTO;
+import com.future.bbetter.member.resource.FriendResource;
 import com.future.bbetter.member.resource.MemberResource;
 import com.future.bbetter.member.service.MemberService;
 
 @RestController
-@RequestMapping("/public")
+@RequestMapping("/api")
 //設定該資源需要什麼權限角色
 //@PreAuthorize("hasRole('ROLE_USER')")
 public class MemberController {
@@ -32,32 +33,55 @@ public class MemberController {
 	@Autowired
 	private MemberResource memberResource;
 	@Autowired
+	private FriendResource friendsResource;
+	@Autowired
 	private MemberService memberService;
 	
 	/**
-	 * 取得會員資訊
+	 * 取得個人會員資訊
 	 * @author Charles
 	 * @date 2017年8月27日 下午7:37:14
 	 */
-	
 	//必須具備USER權限，且該取得的會員資訊必須為會原本人
 	//例:已登入但欲取得他人帳戶資訊，利用該IP查到的會員eamil與當前登入中的Authentication的username做比對
 	@PreAuthorize("hasRole('USER')")
 	@PostAuthorize("returnObject.email == principal.username")
-	@GetMapping("/member/{memberId}")
-	public MemberDTO getMember(@PathVariable Long memberId) throws DataNotFoundException{
-		MemberDTO memberInfo = memberResource.getMember(memberId);
+	@GetMapping("/member/self")
+	public MemberDTO getMember() throws DataNotFoundException{
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+	    String email = auth.getName();
+	    MemberDTO memberInfo = memberResource.getMember(email);
 		return memberInfo;
 	}
 	
-	//只有系統管理者才有權限訪問
-	//@PreAuthorize("hasRole('ADMIN')")
+	/** 
+	 * 取得所有會員
+	 * 只有系統管理者才有權限訪問
+	 * @author Charles
+	 * @date 2017年10月2日 下午11:18:21
+	 */
+	//@PreAuthorize("hasRole('USER')")
+	@PreAuthorize("hasRole('ADMIN')")
 	@GetMapping("/members")
 	public List<MemberDTO> getAllMembers(HttpServletRequest req, HttpServletResponse res) {
 		//取的用戶的當地語言，未來用於i18n
-		Locale userPreferredLocale = req.getLocale();
+		//Locale userPreferredLocale = req.getLocale();
 		return memberResource.getAllMembers();
 	}
+	
+	/**
+	 * 取得該會員所有朋友列表
+	 * @author Charles
+	 * @date 2017年10月2日 下午11:18:10
+	 */
+	@PreAuthorize("hasRole('USER')")
+	@GetMapping("/member/{memberId}/friends")
+	public List<FriendDTO> getFriends(@PathVariable Long memberId) throws DataNotFoundException{
+		List<FriendDTO> friends = friendsResource.getFriends(memberId);
+		return friends;
+	}
+	
+	
 	
 	@RequestMapping(value = "/member/{memberId}", method=RequestMethod.PATCH)
 	public void updateUser(@RequestBody MemberDTO updateMemberDTO){
---
+++ src/main/java/com/future/bbetter/member/model/FriendDTO.java
@@ -0,0 +1,37 @@
+package com.future.bbetter.member.model;
+
+import java.util.Date;
+import java.util.List;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+public @Data @NoArgsConstructor class FriendDTO {
+	
+//	public static FriendDTO fromDTO(MemberDTO memberDTO, List<MemberDTO> friendsDTO) {
+//		FriendDTO friendDTO = new FriendDTO();
+//		friendDTO.setMemberDTO(memberDTO);
+//		friendDTO.setFriendsDTO(friendsDTO);
+//		return friendDTO;
+//	}
+//	
+//	private MemberDTO memberDTO;
+//	private List<MemberDTO> friendsDTO;
+	
+	public static FriendDTO fromEntity(Member member, Friend friend) {
+		FriendDTO friendDTO = new FriendDTO();
+		friendDTO.setName(member.getName());
+		friendDTO.setEmail(member.getEmail());
+		friendDTO.setGender(member.getGender());
+		friendDTO.setBirthday(member.getBirthday());
+		friendDTO.setCreatedate(friend.getCreatedate());
+		return friendDTO;
+	}
+	
+	private String email;
+	private String name;
+	private Integer gender;
+	private Date birthday;
+	private Date createdate;
+	
+}
