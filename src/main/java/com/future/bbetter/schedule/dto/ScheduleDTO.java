815e6a59f8049c24f56e952dfb2b2e83e64ad42b
Charles-PC\Charles <as130232@gmail.com>
2017-10-22 22:37:55 +0800

    chore schedule 更新DTO屬性結構

--- src/main/java/com/future/bbetter/schedule/dto/ScheduleDTO.java
+++ src/main/java/com/future/bbetter/schedule/dto/ScheduleDTO.java
@@ -3,7 +3,6 @@ package com.future.bbetter.schedule.dto;
 import java.util.Date;
 
 import com.future.bbetter.schedule.model.Schedule;
-import com.future.bbetter.schedule.model.ScheduleHad;
 
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -16,56 +15,10 @@ import lombok.NonNull;
  *
  */
 public @Data @NoArgsConstructor class ScheduleDTO {
-
-	/***
-	 * 從entity轉換成ScheduleDTO物件
-	 * @param schedule non null.
-	 * @return ScheduleDTO object
-	 */
-	public static ScheduleDTO from(@NonNull Schedule schedule) {
-		ScheduleDTO scheduleDTO = new ScheduleDTO();
-		if(schedule.getScheduleSubType() != null){
-			scheduleDTO.setType(ScheduleTypeDTO.from(schedule.getScheduleSubType()));
-		}
-		if(schedule.getScheduleId() != null){
-			scheduleDTO.setScheduleId(schedule.getScheduleId());
-		}
-		scheduleDTO.setStartTime(schedule.getStartTime());
-		scheduleDTO.setEndTime(schedule.getEndTime());
-		scheduleDTO.setName(schedule.getName());
-		scheduleDTO.setLocation(schedule.getLocation());
-		scheduleDTO.setStatus(schedule.getStatus());
-		scheduleDTO.setIsCycle(schedule.getIsCycle());
-		scheduleDTO.setIsNeedRemind(schedule.getIsNeedRemind());
-		scheduleDTO.setIsTeamSchedule(schedule.getIsTeamSchedule());
-		scheduleDTO.setCreatedate(schedule.getCreatedate());
-		scheduleDTO.setUpdatedate(schedule.getUpdatedate());
-		return scheduleDTO;
-	}
-	
-	/***
-	 * 將該物件轉成schedule entity 
-	 * @return Schedule object
-	 */
-	public Schedule toSchedule(){
-		Schedule sch = new Schedule();
-		sch.setStartTime(this.getStartTime());
-		sch.setEndTime(this.getEndTime());
-		sch.setName(this.getName());
-		sch.setLocation(this.getLocation());
-		sch.setStatus(this.getStatus());
-		sch.setIsCycle(this.getIsCycle());
-		sch.setIsNeedRemind(this.getIsNeedRemind());
-		sch.setIsTeamSchedule(this.getIsTeamSchedule());
-		if(this.getType() != null){
-			sch.setScheduleSubType(this.getType().toSubType());
-		}
-		return sch;
-	}
-	
-	
-	// Schedule.attributes
+	// schedule.attributes
 	private Long scheduleId;
+	private ScheduleTypeDTO scheduleTypeInfo;
+	private Integer skillId;
 	private Date startTime;
 	private Date endTime;
 	private String name;
@@ -76,16 +29,54 @@ public @Data @NoArgsConstructor class ScheduleDTO {
 	private Integer isCycle;
 	private Integer isNeedRemind;
 	private Integer isTeamSchedule;
-	private Integer isValid;
 	private Date createdate;
-	private Date updatedate;
+	//private Integer isValid;
+	//private Date updatedate;
 	
-	private ScheduleTypeDTO type;
+	/***
+	 * 從entity轉換成ScheduleDTO物件
+	 * @param schedule non null.
+	 * @return ScheduleDTO object
+	 */
+	public static ScheduleDTO fromEntity(@NonNull Schedule schedule) {
+		ScheduleDTO scheduleDTO = new ScheduleDTO();
+		scheduleDTO.setScheduleId(schedule.getScheduleId());
+		scheduleDTO.setSkillId(schedule.getSkillId());
+		scheduleDTO.setStartTime(schedule.getStartTime());
+		scheduleDTO.setEndTime(schedule.getEndTime());
+		scheduleDTO.setName(schedule.getName());
+		scheduleDTO.setLocation(schedule.getLocation());
+		scheduleDTO.setStatus(schedule.getStatus());
+		scheduleDTO.setVisibility(schedule.getVisibility());
+		scheduleDTO.setIsCycle(schedule.getIsCycle());
+		scheduleDTO.setIsNeedRemind(schedule.getIsNeedRemind());
+		scheduleDTO.setIsTeamSchedule(schedule.getIsTeamSchedule());
+		scheduleDTO.setScheduleTypeInfo(ScheduleTypeDTO.fromEntity(schedule.getScheduleType()));
+		scheduleDTO.setCreatedate(schedule.getCreatedate());
+		//scheduleDTO.setIsValid(schedule.getIsValid());
+		//scheduleDTO.setUpdatedate(schedule.getUpdatedate());
+		return scheduleDTO;
+	}
 	
-//	// ScheduleHad.attributes
-//	private Long scheduleHadId;
-//	// 擁有該行程的會員id
-//	private Long ownerId;	
-//	private Integer authority;
-//	private Integer accumulatedTime;
+	/***
+	 * 將該物件轉成schedule entity 
+	 * @return Schedule object
+	 */
+//	public Schedule toSchedule(){
+//		Schedule sch = new Schedule();
+//		sch.setStartTime(this.getStartTime());
+//		sch.setEndTime(this.getEndTime());
+//		sch.setName(this.getName());
+//		sch.setLocation(this.getLocation());
+//		sch.setStatus(this.getStatus());
+//		sch.setIsCycle(this.getIsCycle());
+//		sch.setIsNeedRemind(this.getIsNeedRemind());
+//		sch.setIsTeamSchedule(this.getIsTeamSchedule());
+//		if(this.getType() != null){
+//			sch.setScheduleType(this.getType().toType());
+//		}
+//		return sch;
+//	}
+	
+	
 }
