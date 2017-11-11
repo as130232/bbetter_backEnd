3b8bacebd913b65b3356f8e998a7b06b22519da9
Charles-PC\Charles <as130232@gmail.com>
2017-11-11 14:37:54 +0800

    feat test 新增friendTest

--- src/test/java/com/future/bbetter/member/resource/impl/FriendResourceImplTest.java
+++ src/test/java/com/future/bbetter/member/resource/impl/FriendResourceImplTest.java
@@ -4,6 +4,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -43,14 +44,14 @@ public class FriendResourceImplTest {
 
 	@Autowired
 	private FriendResource friendRs;
-
-	Long memberId = 0L;
-	Long memberIdByFriend = 0L;
+	
+	Member peter;
+	Member john;
 	
 	@Before
 	public void setup(){
 		//先產生兩位會員
-		Member peter = new Member();
+		peter = new Member();
 		peter.setName("peter");
 		peter.setPassword("1234");
 		peter.setAddress("Taiwan");
@@ -58,9 +59,9 @@ public class FriendResourceImplTest {
 		peter.setEmail("peter@bbetter.com");
 		peter.setGender(1);
 		peter.setMoney(new BigDecimal(0));
-		memberId = (Long) entityMgr.persistAndGetId(peter);
+		entityMgr.persist(peter);
 
-		Member john = new Member();
+		john = new Member();
 		john.setName("john");
 		john.setPassword("1234");
 		john.setAddress("Taiwan");
@@ -68,30 +69,88 @@ public class FriendResourceImplTest {
 		john.setEmail("john@bbetter.com");
 		john.setGender(1);
 		john.setMoney(new BigDecimal(0));
-		memberIdByFriend = (Long) entityMgr.persistAndGetId(john);
+		entityMgr.persist(john);
 		
 	}
 	
+	/**
+	 * 測試新增好友(發出好友邀請，但對方未同意>IS_ACCEPT_NO)
+	 * peter 向 john發出好友邀請
+	 * @author Charles
+	 * @date 2017年11月5日 下午6:37:46
+	 */
 	@Test
 	public void whenAddFriend_thenCanFoundOneRecord() {
 		// given
 		Integer source = FRIEND.SOURCE_BBETTER.value;
 		//還未接收好友邀請
 		Integer isAccept = FRIEND.IS_ACCEPT_NO.value;
+		
 		// when
-		FriendDto result = friendRs.addFriend(memberId, memberIdByFriend, source, isAccept);
+		FriendDto result = friendRs.addFriend(peter.getMemberId(), john.getMemberId(), source, isAccept);
 		
 		// then
 		Friend found = entityMgr.find(Friend.class, result.getFriendId());
-		assertThat(found.getMemberByFriendMemberId().getMemberId()).isEqualTo(memberIdByFriend);
+		assertThat(found.getMemberByFriendMemberId().getMemberId()).isEqualTo(john.getMemberId());
 		assertThat(found.getSource()).isEqualTo(source);
 		//無被封鎖
 		Integer isBlockade = FRIEND.IS_BLOCKADE_NO.value;
 		assertThat(found.getIsBlockade()).isEqualTo(isBlockade);
-		
 		assertThat(found.getIsAccept()).isEqualTo(isAccept);
-		
 	}
 	
+
+	/**
+	 * 測試取得該會員還未接受成為好友的會員列表
+	 * john取得還未接受邀請好友的會員列表(peter)
+	 * @author Charles
+	 * @date 2017年11月5日 下午6:37:46
+	 */
+	@Test
+	public void whenGetFriendsInAcceptNotYet_thenCanFoundOneRecord() {
+		// given
+		// peter 向 john發出好友邀請
+		friendRs.addFriend(peter.getMemberId(), john.getMemberId(), 
+				FRIEND.SOURCE_BBETTER.value, FRIEND.IS_ACCEPT_NO.value);
+		
+		// when
+		List<FriendDto> friendDtosInAcceptNotYet = friendRs.getFriendsInAcceptNotYet(john.getMemberId());
+		FriendDto result = friendDtosInAcceptNotYet.get(0);
+		
+		// then
+		Friend found = entityMgr.find(Friend.class, result.getFriendId());
+		Long friendMemberId = found.getMemberByMemberId().getMemberId();
+		assertThat(friendMemberId).isEqualTo(peter.getMemberId());
+		assertThat(found.getIsAccept()).isEqualTo(FRIEND.IS_ACCEPT_NO.value);
+	}
+	
+	/**
+	 * 測試接收好友邀請
+	 * john接收peter 發出的好友邀請，雙方成為好友
+	 * @author Charles
+	 * @date 2017年11月5日 下午6:38:31
+	 */
+	@Test
+	public void whenAcceptFriendApply_thenBothBecomeFriends() {
+		// given
+		// peter 向 john發出好友邀請
+		friendRs.addFriend(peter.getMemberId(), john.getMemberId(), 
+				FRIEND.SOURCE_BBETTER.value, FRIEND.IS_ACCEPT_NO.value);
+		
+		// john 取得還未接受成為好友的會員列表(peter)
+		List<FriendDto> friendDtosInAcceptNotYet = friendRs.getFriendsInAcceptNotYet(john.getMemberId());
+		FriendDto friendDtoInAcceptNotYet = friendDtosInAcceptNotYet.get(0);
+		
+		// john 同意peter的好友邀請
+		// when
+		friendRs.acceptFriendApply(john.getMemberId(), friendDtoInAcceptNotYet.getFriendId());
+		
+		// then
+		Friend found = entityMgr.find(Friend.class, friendDtoInAcceptNotYet.getFriendId());
+		// peter好友列表中john邀請狀態為已接受狀態
+		assertThat(found.getIsAccept()).isEqualTo(FRIEND.IS_ACCEPT_YES.value);
+		
+		// john好友列表中新增了peter且邀請狀態也是已接受狀態
+	}
 }
 	
