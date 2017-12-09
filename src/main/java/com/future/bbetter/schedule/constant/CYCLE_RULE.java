e296098d9a246bd65468520ba7830e3dcf40da03
alfreadx <alfreadx@gmail.com>
2017-12-09 14:59:16 +0000

    feat 新增cycle rule的常數

---
+++ src/main/java/com/future/bbetter/schedule/constant/CYCLE_RULE.java
@@ -0,0 +1,28 @@
+package com.future.bbetter.schedule.constant;
+
+public enum CYCLE_RULE {
+	
+	
+	/** 循環週期: 每日(1) **/
+	PERIOD_DAILY(1, ""),
+	/** 循環週期: 每週(2) **/
+	PERIOD_WEEKLY(2, ""),
+	/** 循環週期: 每月(3) **/
+	PERIOD_MONTHLY(3, ""),
+	/** 循環週期: 每年(4) **/
+	PERIOD_ANNUALLY(4, ""),
+	
+	
+	/** 無效(0) **/
+	IS_VALID_NO(0, "無效"),
+	/** 有效(1) **/
+	IS_VALID_YES(1, "有效");
+	
+	public final Integer value;
+	public final String depiction;
+	private CYCLE_RULE(Integer value, String depiction){
+		this.value = value;
+		this.depiction = depiction;
+	}
+
+}
